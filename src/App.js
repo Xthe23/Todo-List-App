bed7119bef90d6b4a7acf875da86127274e14914
Michael Kravitz <30937928+Xthe23@users.noreply.github.com>
2020-11-04 12:20:15 -0800

    Updated Stylesheets
    Added TailwindCSS to React Project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import './App.css';
+import './assets/main.css'
 
 function App() {
   return (
     <div className="App">
-      <h1>Michael's Todo List</h1>
+      <h1 className="text-5xl">Michael's Todo List</h1>
     </div>
   );
 }
